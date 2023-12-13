74f7eca02b477ea8d0120cae4785c6bb6044520d
manu <manuelpadinpresa@gmx.es>
2023-12-13 12:45:40 +0100

    hecho

--- Tarefa41/Tarefa41.py
+++ Tarefa41/Tarefa41.py
@@ -1,5 +1,13 @@
-import pytest
-import math
+""" Tarefa 41
+
+Similar a exercicios anteriores, podes copiar o feito nesa parte e continuar ampliando:
+Escribir unha función que calcule a área dun círculo e outra que calcule o volume dun cilindro usando a primeira función.
+A area do círculo é pi*r**2
+O volume do cilindro é  pi*r**2 * h
+
+Xera test uniatarios, primeiro para a función que calcula a area do circulo, e logo para o volume do cilindro.
+""" import pytest
+import math """
 
 radio_circulo = int(input('Escriba cuanto mide el radio del circulo :'))
 altura_cilindro = int(input('Escriba cuanto mide la altura del cilindro :'))
@@ -12,5 +20,14 @@ def calcular_volumen_cilindro(radio, altura):
 
     area_base = calcular_area_circulo(radio)
     volumen =  area_base * altura
-    return volumen
+    return volumen """
 
+import math
+
+def calcular_area_circulo(radio):
+    return math.pi * radio**2
+
+def calcular_volumen_cilindro(radio, altura):
+    area_base = calcular_area_circulo(radio)
+    volumen =  area_base * altura
+    return volumen
